5663956b7e17563e558096f2d424db7139f8e853
Paul Jones <bethekind@gmail.com>
2023-06-10 21:19:13 -0700

    Made the mainfile testable. Still too scared to test it yet.

--- TextCleaner.py
+++ TextCleaner.py
@@ -1,5 +1,6 @@
 import re
 
+
 class RemoveAscii:
     def __init__(self):
         self.ascii_pattern = re.compile(r'[^\x00-\x7F]+')
@@ -7,12 +8,14 @@ class RemoveAscii:
     def execute(self, text):
         return self.ascii_pattern.sub(r'', text)
 
+
 class RemoveUrls:
     def __init__(self):
         self.url_pattern = re.compile(r'https?://\S+')
 
     def execute(self, text):
         return self.url_pattern.sub(r'', text)
+
 
 class ReplaceDoubleQuotes:
     def execute(self, text):
